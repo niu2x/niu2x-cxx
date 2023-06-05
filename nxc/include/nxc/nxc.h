c9fac5ab0019d5c426ecc634c6e088fe009e7173
xiaorong.niu <xiaorong.niu@happyelements.com>
2023-06-05 11:31:30 +0800

    rename file

--- nxc/include/nxc/nxc.h
+++ nxc/include/nxc/nxc.h
@@ -3,17 +3,19 @@
 
 #include <nxc/api.h>
 #include <nxc/config.h>
-#include <nxc/error.h>
 #include <nxc/utils.h>
+
+#include <nxc/error.h>
 #include <nxc/result.h>
+
 #include <nxc/buffer.h>
 // #include <nxc/raw_memory.h>
 #include <nxc/stream.h>
 #include <nxc/stream_factory.h>
 #include <nxc/archive_factory.h>
-#include <nxc/pipe.h>
 #include <nxc/null_file.h>
 #include <nxc/lexer.h>
 #include <nxc/curlx.h>
+#include <nxc/pipe.h>
 
 #endif
