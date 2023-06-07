14bc353b70a4ebc1156f2e87258e5ae92792658a
niu2x <niu2x@icloud.com>
2023-06-08 01:37:45 +0800

    fix

--- nxc/include/nxc/stream.h
+++ nxc/include/nxc/stream.h
@@ -49,6 +49,56 @@ private:
     }
 };
 
+template <class Ch, int BUF_SIZE>
+class WriteStream {
+public:
+    using Char = Ch;
+
+    WriteStream() { reset(); }
+
+    virtual ~WriteStream() { }
+
+    Result<void> write(Ch c)
+    {
+        if (should_flush()) {
+            auto ret = flush();
+            if (!ret)
+                return ret;
+        }
+        *(next_++) = c;
+        return E::OK;
+    }
+
+    void reset() { next_ = begin_ = buf_; }
+
+    Result<size_t> flush()
+    {
+        auto ret = _flush(begin_, next_ - begin_);
+        if (ret) {
+            begin_ += *ret;
+            if (next_ - begin_ > 0) {
+                auto count = next_ - begin_;
+                memmove(buf_, begin_, (next_ - begin_) * sizeof(Char));
+                begin_ = buf_;
+                next_ = count + buf_;
+            } else {
+                next_ = begin_ = buf_;
+            }
+        }
+        return ret;
+    }
+
+protected:
+    virtual Result<size_t> _flush(Char* ptr, size_t count) = 0;
+
+private:
+    Char buf_[BUF_SIZE];
+    Char* next_;
+    Char* begin_;
+
+    bool should_flush() { return next_ >= buf_ + BUF_SIZE; }
+};
+
 class NXC_API FileReadStream : public ReadStream<uint8_t, 1024> {
 public:
     FileReadStream();
