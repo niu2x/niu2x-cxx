27f1dcc67cca149449e177d711fbf9e08f4bcc8c
xiaorong.niu <xiaorong.niu@happyelements.com>
2023-06-12 08:20:22 +0800

    luaing

--- nxc/include/nxc/lua.h
+++ nxc/include/nxc/lua.h
@@ -3,16 +3,6 @@
 
 namespace nxc {
 
-union GCObject;
-#define CommonHeader                                                           \
-    GCObject* next;                                                            \
-    uint8_t tt;                                                                \
-    uint8_t marked
-
-struct GCheader {
-    CommonHeader;
-};
-
 } // namespace nxc
 
 #endif
