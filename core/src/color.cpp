1781788704348606bc17bb2bb301bcff22c8938d
niu2x <niu2x@icloud.com>
2023-10-06 21:56:51 +0800

    add named color

--- core/src/color.cpp
+++ core/src/color.cpp
@@ -1,12 +1,7 @@
 #include <niu2x/type/color.h>
 #include <niu2x/type/std_alias.h>
 
-// #define NIU2X_COLOR_CLAMP(n) max(min((int)(n), 255), 0)
-
 namespace niu2x {
-
-const Color Color::WHITE = Color(255, 255, 255, 255);
-const Color Color::BLACK = Color(0, 0, 0, 255);
 
 Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 : r(r)
@@ -20,13 +15,5 @@ Color::Color()
 : rgba8(0)
 {
 }
-
-// Color::Color(const Vec3& c)
-// : r(NIU2X_COLOR_CLAMP(c.x * 255))
-// , g(NIU2X_COLOR_CLAMP(c.y * 255))
-// , b(NIU2X_COLOR_CLAMP(c.z * 255))
-// , a(255)
-// {
-// }
 
 } // namespace niu2x
