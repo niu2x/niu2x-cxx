3151f77858201fab3d87acbc789ce35dd314e670
niu2x <niu2x@icloud.com>
2023-10-20 01:17:12 +0800

    add json

--- core/include/niu2x/json.h
+++ core/include/niu2x/json.h
@@ -4,6 +4,14 @@
 namespace niu2x {
 
 class JSON {
+public:
+    bool is_number();
+    bool is_boolean();
+    bool is_string();
+    bool is_object();
+    bool is_array();
+
+private:
 };
 
 } // namespace niu2x
