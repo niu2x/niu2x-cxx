aa249d1b1c48e6040471dff5ea756c95a7caaf13
niu2x <niu2x@icloud.com>
2023-11-06 21:39:58 +0800

    gfx gui

--- app/poe/main.cpp
+++ app/poe/main.cpp
@@ -42,26 +42,8 @@ public:
         res_mgr->load_render_program(gfx::RenderProgramID::TEXTURE_COLOR);
         res_mgr->load_render_program(gfx::RenderProgramID::COLOR);
 
-        render_program_
-            = res_mgr->get_render_program(gfx::RenderProgramID::TEXTURE_COLOR);
-
-        gfx::Vertex triangle[] = {
-            { 0, 0, 0, 1, 1, 1, 1, 0, 0 },
-            { 1, 0, 0, 1, 1, 1, 1, 1, 0 },
-            { 1, 1, 0, 1, 1, 1, 1, 1, 1 },
-
-            { 0, 0, 0, 1, 1, 1, 1, 0, 0 },
-            { 1, 1, 0, 1, 1, 1, 1, 1, 1 },
-            { 0, 1, 0, 1, 1, 1, 1, 0, 1 },
-
-        };
-
-        vertex_buffer_ = gfx_factory->create_vertex_buffer();
-        vertex_buffer_->resize(6);
-        vertex_buffer_->set_vertexs(0, 6, triangle);
 
         res_mgr->load_texture2d("resource/image/test_00.yml");
-        tex_ = res_mgr->get_texture2d("resource/image/test_00.yml");
 
         res_mgr->load_vertex_buffer("resource/vertex_buffer/square.yml");
 
@@ -99,26 +81,6 @@ public:
             gfx::RenderCommandFactory::get()->create_clear());
         gui_root_->draw();
         gfx::render();
-        // gfx::Draw::clear();
-        // vertex_buffer_->bind();
-        // tex_->bind(0);
-        // render_program_->bind();
-        // render_program_->set_uniform_integer(
-        //     gfx::RenderProgram::Uniform::TEX_0, 0);
-
-        // gfx::Mat4 unit_mat4 { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 },
-        //     { 0, 0, 0, 1 } };
-
-        // render_program_->set_uniform_mat4(
-        //     gfx::RenderProgram::Uniform::MODEL, unit_mat4);
-
-        // render_program_->set_uniform_mat4(
-        //     gfx::RenderProgram::Uniform::VIEW, unit_mat4);
-
-        // render_program_->set_uniform_mat4(
-        //     gfx::RenderProgram::Uniform::PROJECTION, unit_mat4);
-
-        // gfx::Draw::draw_triangles(0, 6);
     }
 
     void on_resize(gfx::IntSize window_size) override
@@ -128,9 +90,6 @@ public:
     }
 
 private:
-    gfx::RenderProgram* render_program_;
-    UniquePtr<gfx::VertexBuffer> vertex_buffer_;
-    gfx::Texture2D* tex_;
     UniquePtr<gfx::gui::Node> gui_root_;
 };
 
