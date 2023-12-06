39b00a067c3ff1f4c2ea940de3478fe726e0b4aa
niu2x <niu2x@icloud.com>
2023-12-07 00:35:24 +0800

    refactor

--- filesystem/src/exists.cpp
+++
@@ -1,9 +0,0 @@
-#include <niu2x/fs.h>
-#include <filesystem>
-#include "build_time/build_config.h"
-
-namespace niu2x::fs {
-
-bool exists(const Path& path) { return std::filesystem::exists(path.native()); }
-
-} // namespace niu2x::fs
